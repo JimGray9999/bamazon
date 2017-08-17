877bb6f915717c9076b8b41396e9f8bedbac44cc
Jim Gray <JimGray9999@users.noreply.github.com>
2017-08-17 07:29:01 -0400

    updates

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,12 +22,15 @@ connection.connect(function(err){
 
 function startApp() {
   var query = "SELECT id, product_name, price FROM products";
+  
   var showTheGoods = connection.query(query, function(err,data) {
       if (err) throw err;
       console.log("********************");
       console.log("*     BAMAZON!     *");
       console.log("********************");
 
+      // prints out the table of products in a clean row
+      // using the console.table npm package
       console.table(data);
   inquirer
   .prompt([
@@ -38,19 +41,27 @@ function startApp() {
     },
     {
       type: "input",
-      message: "Enter how many to buy: ",
+      message:  "Enter how many to buy: ",
       name: "quantity"
     },
     {
+      // confirm if customer is ready to place order
       type: "confirm",
       message: "Confirm order?",
       name: "doIt"
     }
   ]).then(function(inq) {
-    processOrder(inq.product, inq.quantity);
+    if (inq.doIt === true){
+      processOrder(inq.product, inq.quantity); // place order
+    } else {
+      console.log("Returning to main menu...");
+      startApp(); // returns to main menu
+    }
   })
 })
 
+  // after customer confirms order, quantity is checked
+  // order placed if quantity is available
   function processOrder(id, quantity) {
     let query = "SELECT id, product_name, price, stock_quantity FROM products WHERE ?"
     connection.query(query,{id: id}, function(err, data) {
@@ -61,17 +72,38 @@ function startApp() {
         let query = "UPDATE products SET stock_quantity = stock_quantity - ?";
         query += " WHERE id = ?";
         connection.query(query,[quantity, id], function(err, data){
+          
           console.log("Order Placed!");
           console.log("Purchased " + quantity + "  " + productName);
           console.log("Total Cost: " + "$" + cost);
-          startApp();
+
+          inquirer
+          .prompt([
+            {
+              type: "confirm",
+              message: "Place another order?",
+              name: "nextOrder"
+            }
+          ]).then(function(inq) {
+            if(inq.nextOrder === true){
+              startApp(); // reload application after successful purchase
+            } else {
+              // close application if customer is finished
+              console.log("Thank you for choosing Bamazon for your shopping needs!");
+              
+              // close connection to the database
+              connection.end();
+            }
+          })
         })
       } else {
-        console.log("Insuffient Inventory, try again or contact a Bamazon Manager");
-        startApp();
+        // display message if not enough inventory
+        console.log("Insuffient inventory, try again or contact a Bamazon Manager");
+        startApp(); // returns to the main menu
       }
     })
   }
 }
 
+// launches main menu when file is opened
 startApp();
