dc44d5ed681820a7869c5d32f257ae92d3e4ac5b
Jim Gray <jimgray2005@gmail.com>
2018-08-08 13:32:38 -0400

    added to another computer

--- bamazon-seeds.sql
+++ bamazon-seeds.sql
@@ -1,31 +1,31 @@
 
 -- populate database with 10 initial product inventory items --
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Fidget Spinners", "Toys", 10.00, 500);
+INSERT INTO products(id, product_name, department_name, price, stock_quantity)
+VALUES (0, "Fidget Spinners", "Toys", 10.00, 500);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Rubber Ducks", "Toys", 5.00, 7500);
+INSERT INTO products(id, product_name, department_name, price, stock_quantity)
+VALUES (1, "Rubber Ducks", "Toys", 5.00, 7500);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Bamazon Chambers", "Gadgets", 159.95, 3500);
+INSERT INTO products(id, product_name, department_name, price, stock_quantity)
+VALUES (2, "Bamazon Chambers", "Gadgets", 159.95, 3500);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Bamazon Firestarters", "eReaders", 50.00, 1000);
+INSERT INTO products(id, product_name, department_name, price, stock_quantity)
+VALUES (3, "Bamazon Firestarters", "eReaders", 50.00, 1000);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Canned Stuffed Grape Leaves", "Food", 2.99, 425);
+INSERT INTO products(id, product_name, department_name, price, stock_quantity)
+VALUES (4, "Canned Stuffed Grape Leaves", "Food", 2.99, 425);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Laser Pointers", "Business", 14.99, 575);
+INSERT INTO products(id, product_name, department_name, price, stock_quantity)
+VALUES (5, "Laser Pointers", "Business", 14.99, 575);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Red Swingline Stapler", "Business", 7549.95, 2);
+INSERT INTO products(id, product_name, department_name, price, stock_quantity)
+VALUES (6, "Red Swingline Stapler", "Business", 7549.95, 2);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Foreigner Greatest Hits", "Music", 11.99, 3450);
+INSERT INTO products(id, product_name, department_name, price, stock_quantity)
+VALUES (7, "Foreigner Greatest Hits", "Music", 11.99, 3450);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("The 1986 New York Mets", "Documentaries", 9.99, 100);
+INSERT INTO products(id, product_name, department_name, price, stock_quantity)
+VALUES (8, "The 1986 New York Mets", "Documentaries", 9.99, 100);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Organic Catnip", "Pets", 12.95, 400);
+INSERT INTO products(id, product_name, department_name, price, stock_quantity)
+VALUES (9, "Organic Catnip", "Pets", 12.95, 400);
